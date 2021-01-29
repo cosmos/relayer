4d1a7a5d4afabebb21262eb6bc9435e3a5cb028d
NagaTulasi <tulasitulip@gmail.com>
2021-01-29 17:22:11 +0530

    Sprint issue resolved

--- relayer/channel-tx.go
+++ relayer/channel-tx.go
@@ -62,7 +62,7 @@ func (c *Chain) CreateOpenChannels(dst *Chain, maxRetries uint64, to time.Durati
 		// In the case of failure, increment the failures counter and exit if this is the 3rd failure
 		case !success:
 			failures++
-			str := fmt.Sprint("retrying transaction...")
+			str := "retrying transaction..."
 			c.Log(str)
 			time.Sleep(5 * time.Second)
 
