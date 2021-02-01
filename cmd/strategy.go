3a7de6d4747619f9230b9345b3f1332e9735fce4
NagaTulasi <tulasitulip@gmail.com>
2021-02-01 11:14:30 +0530

    start changed to sTrategy

--- cmd/strategy.go
+++ cmd/strategy.go
@@ -9,35 +9,35 @@ import (
 
 // GetStrategyWithOptions sets strategy specific fields.
 func GetStrategyWithOptions(cmd *cobra.Command, strategy relayer.Strategy) (relayer.Strategy, error) {
-	switch start := strategy.(type) {
+	switch sTrategy := strategy.(type) {
 	case *relayer.NaiveStrategy:
 		maxTxSize, err := cmd.Flags().GetString(flagMaxTxSize)
 		if err != nil {
-			return start, err
+			return sTrategy, err
 		}
 
 		txSize, err := strconv.ParseUint(maxTxSize, 10, 64)
 		if err != nil {
-			return start, err
+			return sTrategy, err
 		}
 
 		// set max size of messages in a relay transaction
-		start.MaxTxSize = txSize * MB // in MB
+		sTrategy.MaxTxSize = txSize * MB // in MB
 
 		maxMsgLength, err := cmd.Flags().GetString(flagMaxMsgLength)
 		if err != nil {
-			return start, err
+			return sTrategy, err
 		}
 
 		msgLen, err := strconv.ParseUint(maxMsgLength, 10, 64)
 		if err != nil {
-			return start, err
+			return sTrategy, err
 		}
 
 		// set max length messages in relay transaction
-		start.MaxMsgLength = msgLen
+		sTrategy.MaxMsgLength = msgLen
 
-		return start, nil
+		return sTrategy, nil
 	default:
 		return strategy, nil
 	}
