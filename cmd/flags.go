39bc53cc3ba951e35fe8b22e7eae55f427c6d708
NagaTulasi <tulasitulip@gmail.com>
2021-02-01 22:42:54 +0530

    issues fixed

--- cmd/flags.go
+++ cmd/flags.go
@@ -197,8 +197,8 @@ func versionFlag(cmd *cobra.Command) *cobra.Command {
 }
 
 func forceFlag(cmd *cobra.Command) *cobra.Command {
-	cmd.Flags().BoolP(flagForce, "f", false,
-		"option to force non-standard behavior such as initialization of light client from configured chain or generation of new path") //nolint:lll
+	//nolint:lll
+	cmd.Flags().BoolP(flagForce, "f", false, "option to force non-standard behavior such as initialization of light client from configured chain or generation of new path")
 	if err := viper.BindPFlag(flagForce, cmd.Flags().Lookup(flagForce)); err != nil {
 		panic(err)
 	}
