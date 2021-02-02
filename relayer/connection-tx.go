a26cc6572dfb98ca4ff8ecd5328b3eaeed3f423f
NagaTulasi <tulasitulip@gmail.com>
2021-02-02 11:34:56 +0530

    issues fixed

--- relayer/connection-tx.go
+++ relayer/connection-tx.go
@@ -233,8 +233,8 @@ func ExecuteConnectionStep(src, dst *Chain) (success, last, modified bool, err e
 // The identifiers set in the PathEnd's are used to determine which connection ends need to be
 // initialized. The PathEnds are updated upon a successful transaction.
 // NOTE: This function may need to be called twice if neither connection exists.
-//nolint:lll
-func InitializeConnection(src, dst *Chain, srcUpdateHeader, dstUpdateHeader *tmclient.Header, sh *SyncHeaders) (success, modified bool, err error) {
+func InitializeConnection(src, dst *Chain, srcUpdateHeader, dstUpdateHeader *tmclient.Header,
+	sh *SyncHeaders) (success, modified bool, err error) {
 	switch {
 
 	// OpenInit on source
@@ -369,12 +369,14 @@ func FindMatchingConnection(source, counterparty *Chain) (string, bool) {
 // IsMatchingConnection determines if given connection matches required conditions
 func IsMatchingConnection(source, counterparty *Chain, connection *conntypes.IdentifiedConnection) bool {
 	// determines version we use is matching with given versions
-	_, isVersionMatched := conntypes.FindSupportedVersion(conntypes.DefaultIBCVersion, conntypes.ProtoVersionsToExported(connection.Versions))
+	_, isVersionMatched := conntypes.FindSupportedVersion(conntypes.DefaultIBCVersion,
+		conntypes.ProtoVersionsToExported(connection.Versions))
 	return connection.ClientId == source.PathEnd.ClientID &&
 		connection.Counterparty.ClientId == counterparty.PathEnd.ClientID &&
 		isVersionMatched && connection.DelayPeriod == defaultDelayPeriod &&
 		connection.Counterparty.Prefix.String() == defaultChainPrefix.String() &&
-		(((connection.State == conntypes.INIT || connection.State == conntypes.TRYOPEN) && connection.Counterparty.ConnectionId == "") ||
+		(((connection.State == conntypes.INIT || connection.State == conntypes.TRYOPEN) &&
+			connection.Counterparty.ConnectionId == "") ||
 			(connection.State == conntypes.OPEN && (counterparty.PathEnd.ConnectionID == "" ||
 				connection.Counterparty.ConnectionId == counterparty.PathEnd.ConnectionID)))
 }
