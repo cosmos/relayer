5f3a2e17651fe2160823187006b4fe265d902e12
justintieri <justin@thetieris.com>
2022-03-21 15:57:40 -0500

    make functions in `strategies.go` private since we only use them in the package

--- relayer/strategies.go
+++ relayer/strategies.go
@@ -29,7 +29,7 @@ func StartRelayer(ctx context.Context, src, dst *Chain, filter *ChannelFilter, m
 				return
 			default:
 				// Query the list of channels on the src connection
-				srcChannels, err := QueryChannelsOnConnection(src)
+				srcChannels, err := queryChannelsOnConnection(src)
 				if err != nil {
 					errorChan <- fmt.Errorf("error querying all channels on chain{%s}@connection{%s}: %v \n",
 						src.ChainID(), src.ConnectionID(), err)
@@ -37,16 +37,16 @@ func StartRelayer(ctx context.Context, src, dst *Chain, filter *ChannelFilter, m
 				}
 
 				// Apply the channel filter rule (i.e. build allowlist, denylist or relay on all channels available)
-				srcChannels = ApplyChannelFilterRule(filter, srcChannels)
+				srcChannels = applyChannelFilterRule(filter, srcChannels)
 
 				// Filter for open channels that are not already in our slice of open channels
-				srcOpenChannels = FilterOpenChannels(srcChannels, srcOpenChannels)
+				srcOpenChannels = filterOpenChannels(srcChannels, srcOpenChannels)
 
 				// Spin up a goroutine to relay packets & acks for each channel that isn't already being relayed against
 				for _, channel := range srcOpenChannels {
 					if !channel.active {
 						channel.active = true
-						go RelayUnrelayedPacketsAndAcks(ctx, src, dst, maxTxSize, maxMsgLength, channel, channels)
+						go relayUnrelayedPacketsAndAcks(ctx, src, dst, maxTxSize, maxMsgLength, channel, channels)
 					}
 				}
 
@@ -68,8 +68,8 @@ func StartRelayer(ctx context.Context, src, dst *Chain, filter *ChannelFilter, m
 	return errorChan
 }
 
-// QueryChannelsOnConnection queries all the channels associated with a connection on the src chain.
-func QueryChannelsOnConnection(src *Chain) ([]*types.IdentifiedChannel, error) {
+// queryChannelsOnConnection queries all the channels associated with a connection on the src chain.
+func queryChannelsOnConnection(src *Chain) ([]*types.IdentifiedChannel, error) {
 	// Query the latest heights on src & dst
 	srch, err := src.ChainProvider.QueryLatestHeight()
 	if err != nil {
@@ -91,9 +91,9 @@ func QueryChannelsOnConnection(src *Chain) ([]*types.IdentifiedChannel, error) {
 	return srcChannels, nil
 }
 
-// FilterOpenChannels takes a slice of channels and adds all the channels with OPEN state to a new slice of channels.
+// filterOpenChannels takes a slice of channels and adds all the channels with OPEN state to a new slice of channels.
 // NOTE: channels will not be added to the slice of open channels more than once.
-func FilterOpenChannels(channels []*types.IdentifiedChannel, openChannels []*ActiveChannel) []*ActiveChannel {
+func filterOpenChannels(channels []*types.IdentifiedChannel, openChannels []*ActiveChannel) []*ActiveChannel {
 
 	// Filter for open channels
 	for _, channel := range channels {
@@ -121,9 +121,9 @@ func FilterOpenChannels(channels []*types.IdentifiedChannel, openChannels []*Act
 	return openChannels
 }
 
-// ApplyChannelFilterRule will use the given ChannelFilter's rule and channel list to build the appropriate list of
+// applyChannelFilterRule will use the given ChannelFilter's rule and channel list to build the appropriate list of
 // channels to relay on.
-func ApplyChannelFilterRule(filter *ChannelFilter, channels []*types.IdentifiedChannel) []*types.IdentifiedChannel {
+func applyChannelFilterRule(filter *ChannelFilter, channels []*types.IdentifiedChannel) []*types.IdentifiedChannel {
 	switch filter.Rule {
 	case allowList:
 		var filteredChans []*types.IdentifiedChannel
@@ -148,8 +148,8 @@ func ApplyChannelFilterRule(filter *ChannelFilter, channels []*types.IdentifiedC
 	}
 }
 
-// RelayUnrelayedPacketsAndAcks will relay all the pending packets and acknowledgements on both the src and dst chains.
-func RelayUnrelayedPacketsAndAcks(ctx context.Context, src, dst *Chain, maxTxSize, maxMsgLength uint64, srcChannel *ActiveChannel, channels chan<- *ActiveChannel) {
+// relayUnrelayedPacketsAndAcks will relay all the pending packets and acknowledgements on both the src and dst chains.
+func relayUnrelayedPacketsAndAcks(ctx context.Context, src, dst *Chain, maxTxSize, maxMsgLength uint64, srcChannel *ActiveChannel, channels chan<- *ActiveChannel) {
 	// make goroutine signal its death, whether it's a panic or a return
 	defer func() {
 		channels <- srcChannel
@@ -160,10 +160,10 @@ func RelayUnrelayedPacketsAndAcks(ctx context.Context, src, dst *Chain, maxTxSiz
 		case <-ctx.Done():
 			return
 		default:
-			if err := RelayUnrelayedPackets(ctx, src, dst, maxTxSize, maxMsgLength, srcChannel.channel); err != nil {
+			if err := relayUnrelayedPackets(ctx, src, dst, maxTxSize, maxMsgLength, srcChannel.channel); err != nil {
 				return
 			}
-			if err := RelayUnrelayedAcks(ctx, src, dst, maxTxSize, maxMsgLength, srcChannel.channel); err != nil {
+			if err := relayUnrelayedAcks(ctx, src, dst, maxTxSize, maxMsgLength, srcChannel.channel); err != nil {
 				return
 			}
 
@@ -172,8 +172,8 @@ func RelayUnrelayedPacketsAndAcks(ctx context.Context, src, dst *Chain, maxTxSiz
 	}
 }
 
-// RelayUnrelayedPackets fetches unrelayed packet sequence numbers and attempts to relay the associated packets.
-func RelayUnrelayedPackets(ctx context.Context, src, dst *Chain, maxTxSize, maxMsgLength uint64, srcChannel *types.IdentifiedChannel) error {
+// relayUnrelayedPackets fetches unrelayed packet sequence numbers and attempts to relay the associated packets.
+func relayUnrelayedPackets(ctx context.Context, src, dst *Chain, maxTxSize, maxMsgLength uint64, srcChannel *types.IdentifiedChannel) error {
 	childCtx, cancel := context.WithTimeout(ctx, 45*time.Second)
 	defer cancel()
 
@@ -216,8 +216,8 @@ func RelayUnrelayedPackets(ctx context.Context, src, dst *Chain, maxTxSize, maxM
 	return nil
 }
 
-// RelayUnrelayedAcks fetches unrelayed acknowledgements and attempts to relay them.
-func RelayUnrelayedAcks(ctx context.Context, src, dst *Chain, maxTxSize, maxMsgLength uint64, srcChannel *types.IdentifiedChannel) error {
+// relayUnrelayedAcks fetches unrelayed acknowledgements and attempts to relay them.
+func relayUnrelayedAcks(ctx context.Context, src, dst *Chain, maxTxSize, maxMsgLength uint64, srcChannel *types.IdentifiedChannel) error {
 	childCtx, cancel := context.WithTimeout(ctx, 45*time.Second)
 	defer cancel()
 
