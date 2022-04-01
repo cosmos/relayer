3bf75d7b45fabc011741d8b952fe4794b4e463c8
justintieri <justin@thetieris.com>
2022-04-01 16:39:49 -0500

    add tests for `OrderFromString()` and `StringFromOrder()`

---
+++ relayer/pathEnd_test.go
@@ -0,0 +1,41 @@
+package relayer
+
+import (
+	chantypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestOrderFromString(t *testing.T) {
+	const (
+		ordered   = "ordered"
+		unordered = "unordered"
+		none      = ""
+	)
+
+	o := OrderFromString(ordered)
+	require.Equal(t, chantypes.ORDERED, o)
+
+	u := OrderFromString(unordered)
+	require.Equal(t, chantypes.UNORDERED, u)
+
+	empty := OrderFromString(none)
+	require.Equal(t, chantypes.NONE, empty)
+}
+
+func TestStringFromOrder(t *testing.T) {
+	const (
+		ordered   = chantypes.ORDERED
+		unordered = chantypes.UNORDERED
+		none      = chantypes.NONE
+	)
+
+	o := StringFromOrder(ordered)
+	require.Equal(t, "ordered", o)
+
+	u := StringFromOrder(unordered)
+	require.Equal(t, "unordered", u)
+
+	empty := StringFromOrder(none)
+	require.Equal(t, "", empty)
+}
