74c92f54c57355ca170d50527f8a4c123dfe6b36
Justin Tieri <37750742+jtieri@users.noreply.github.com>
2022-04-06 16:08:03 -0500

    Merge PR #679: Properly compose timeouts in `SendTransferMsg` and properly check timestamp in `relayPacketsFromResultTx`
    * properly set the timestamp timeout in `SendTransferMsg()`
    
    * use block time instead of consensus state time when checking for timestamp timeouts in `relayPacketsFromResultTx`
    
    * remove check on time being before epoch time & add perm link in comment

--- relayer/packet-tx.go
+++ relayer/packet-tx.go
@@ -2,10 +2,13 @@ package relayer
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
 	chantypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
 	"github.com/cosmos/relayer/v2/relayer/provider"
 	"go.uber.org/zap"
 )
@@ -13,6 +16,11 @@ import (
 //nolint:lll
 // SendTransferMsg initiates an ics20 transfer from src to dst with the specified args
 func (c *Chain) SendTransferMsg(ctx context.Context, log *zap.Logger, dst *Chain, amount sdk.Coin, dstAddr string, toHeightOffset uint64, toTimeOffset time.Duration, srcChannel *chantypes.IdentifiedChannel) error {
+	var (
+		timeoutHeight    uint64
+		timeoutTimestamp uint64
+	)
+
 	// get header representing dst to check timeouts
 	dsth, err := dst.ChainProvider.QueryLatestHeight(ctx)
 	if err != nil {
@@ -23,21 +31,48 @@ func (c *Chain) SendTransferMsg(ctx context.Context, log *zap.Logger, dst *Chain
 		return err
 	}
 
-	var (
-		timeoutHeight    uint64
-		timeoutTimestamp uint64
-	)
+	// if the timestamp offset is set we need to query the dst chains consensus state to get the current time
+	var consensusState ibcexported.ConsensusState
+	if toTimeOffset > 0 {
+		clientStateRes, err := dst.ChainProvider.QueryClientStateResponse(ctx, dsth, dst.ClientID())
+		if err != nil {
+			return fmt.Errorf("failed to query the client state response: %w", err)
+		}
+		clientState, err := clienttypes.UnpackClientState(clientStateRes.ClientState)
+		if err != nil {
+			return fmt.Errorf("failed to unpack client state: %w", err)
+		}
+		consensusStateRes, err := dst.ChainProvider.QueryClientConsensusState(ctx, dsth, dst.ClientID(), clientState.GetLatestHeight())
+		if err != nil {
+			return fmt.Errorf("failed to query client consensus state: %w", err)
+		}
+		consensusState, err = clienttypes.UnpackConsensusState(consensusStateRes.ConsensusState)
+		if err != nil {
+			return fmt.Errorf("failed to unpack consensus state: %w", err)
+		}
+
+		// use local clock time as reference time if it is later than the
+		// consensus state timestamp of the counter party chain, otherwise
+		// still use consensus state timestamp as reference.
+		// see https://github.com/cosmos/ibc-go/blob/ccc4cb804843f1a80acfb0d4dbf106d1ff2178bb/modules/apps/transfer/client/cli/tx.go#L94-L110
+		tmpNow := time.Now().UnixNano()
+		consensusTimestamp := consensusState.GetTimestamp()
+		now := uint64(tmpNow)
+		if now > consensusTimestamp {
+			timeoutTimestamp = now + uint64(toTimeOffset)
+		} else {
+			timeoutTimestamp = consensusTimestamp + uint64(toTimeOffset)
+		}
+	}
 
 	switch {
 	case toHeightOffset > 0 && toTimeOffset > 0:
 		timeoutHeight = h.GetHeight().GetRevisionHeight() + toHeightOffset
-		timeoutTimestamp = uint64(time.Now().Add(toTimeOffset).UnixNano())
 	case toHeightOffset > 0:
 		timeoutHeight = h.GetHeight().GetRevisionHeight() + toHeightOffset
 		timeoutTimestamp = 0
 	case toTimeOffset > 0:
 		timeoutHeight = 0
-		timeoutTimestamp = uint64(time.Now().Add(toTimeOffset).UnixNano())
 	case toHeightOffset == 0 && toTimeOffset == 0:
 		timeoutHeight = h.GetHeight().GetRevisionHeight() + 1000
 		timeoutTimestamp = 0
