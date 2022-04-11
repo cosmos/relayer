10143b50c49b2893fc74774ef8b04ad9b007b4a5
Mark Rushakoff <mark.rushakoff@gmail.com>
2022-04-11 17:01:40 -0400

    Log gas, fees paid, and fee payer in successful cosmos txs
    This ought to be a useful detail to anyone observing logs, on where the
    funds are being spent.

--- relayer/provider/cosmos/log.go
+++ relayer/provider/cosmos/log.go
@@ -1,7 +1,12 @@
 package cosmos
 
 import (
+	"reflect"
+
+	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	typestx "github.com/cosmos/cosmos-sdk/types/tx"
+	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
 	"github.com/cosmos/relayer/v2/relayer/provider"
 	"go.uber.org/zap"
 )
@@ -33,9 +38,34 @@ func (cc *CosmosProvider) LogFailedTx(res *provider.RelayerTxResponse, err error
 
 // LogSuccessTx take the transaction and the messages to create it and logs the appropriate data
 func (cc *CosmosProvider) LogSuccessTx(res *sdk.TxResponse, msgs []provider.RelayerMessage) {
+	feesField := zap.Skip()
+	feePayerField := zap.Skip()
+
+	ir := types.NewInterfaceRegistry()
+	var m sdk.Msg
+	if err := ir.UnpackAny(res.Tx, &m); err == nil {
+		if tx, ok := m.(*typestx.Tx); ok {
+			feesField = zap.Stringer("fees", tx.GetFee())
+
+			if feePayer := getFeePayer(tx); feePayer != "" {
+				feePayerField = zap.String("fee_payer", feePayer)
+			}
+		} else {
+			cc.log.Debug(
+				"Failed to convert message to Tx type",
+				zap.Stringer("type", reflect.TypeOf(m)),
+			)
+		}
+	} else {
+		cc.log.Debug("Failed to unpack response Tx into sdk.Msg", zap.Error(err))
+	}
+
 	cc.log.Info(
 		"Successful transaction",
 		zap.String("chain_id", cc.ChainId()),
+		zap.Int64("gas_used", res.GasUsed),
+		feesField,
+		feePayerField,
 		zap.Int64("height", res.Height),
 		msgTypesField(msgs),
 		zap.String("tx_hash", res.TxHash),
@@ -48,4 +78,23 @@ func msgTypesField(msgs []provider.RelayerMessage) zap.Field {
 		msgTypes[i] = m.Type()
 	}
 	return zap.Strings("msg_types", msgTypes)
+}
+
+// getFeePayer returns the bech32 address of the fee payer of a transaction.
+// This uses the fee payer field if set,
+// otherwise falls back to the address of whoever signed the first message.
+func getFeePayer(tx *typestx.Tx) string {
+	payer := tx.AuthInfo.Fee.Payer
+	if payer != "" {
+		return payer
+	}
+
+	switch firstMsg := tx.GetMsgs()[0].(type) {
+	case *clienttypes.MsgUpdateClient:
+		// Without this particular special case, there is a panic in ibc-go
+		// due to the sdk config singleton expecting one bech32 prefix but seeing another.
+		return firstMsg.Signer
+	default:
+		return firstMsg.GetSigners()[0].String()
+	}
 }
