68d6be75e4915d52503b68a3550446c74b641fcb
Dan Kanefsky <daniel.kanefsky@gmail.com>
2022-05-17 11:41:08 -0700

    keys.go use chain_name

--- cmd/errors.go
+++ cmd/errors.go
@@ -13,6 +13,10 @@ func errKeyDoesntExist(name string) error {
 	return fmt.Errorf("a key with name %s doesn't exist", name)
 }
 
+func errChainNotFound(chainName string) error {
+	return fmt.Errorf("chain \"%s\" not found in config", chainName)
+}
+
 var (
 	errMultipleAddFlags = errors.New("expected either --file/-f OR --url/u, found multiple")
 )
