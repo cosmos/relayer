4519bed9473c71c35dd79e3c5327dc5c11c8cb74
justintieri <justin@thetieris.com>
2022-06-02 13:24:52 -0500

    add functionality for opening interchain accounts channels

--- relayer/strategies.go
+++ relayer/strategies.go
@@ -52,6 +52,23 @@ func relayerMainLoop(ctx context.Context, log *zap.Logger, src, dst *Chain, filt
 			// Apply the channel filter rule (i.e. build allowlist, denylist or relay on all channels available)
 			srcChannels = applyChannelFilterRule(filter, srcChannels)
 
+			// Ugly way to handle opening a channel for interchain accounts
+			for _, c := range srcChannels {
+				if c.State == types.INIT {
+					_, err := src.CreateOpenChannels(ctx, dst, 10, 5*time.Second, c.PortId, c.Counterparty.PortId, StringFromOrder(c.Ordering), c.Version, false)
+					if err != nil {
+						src.log.Warn("Failed to open channel",
+							zap.String("src-channel-id", c.ChannelId),
+							zap.String("src-port-id", c.PortId),
+							zap.String("dst-channel-id", c.Counterparty.ChannelId),
+							zap.String("dst-port-id", c.Counterparty.ChannelId),
+							zap.String("channel-version", c.Version),
+							zap.String("channel-ordering", c.Ordering.String()),
+							zap.Error(err))
+					}
+				}
+			}
+
 			// Filter for open channels that are not already in our slice of open channels
 			srcOpenChannels = filterOpenChannels(srcChannels, srcOpenChannels)
 		}
