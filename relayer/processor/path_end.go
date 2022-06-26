5399050bf936905fc8430143c694e31300f95069
Andrew Gouin <andrew@gouin.io>
2022-06-26 00:09:35 -0600

    channel, connection, and client filtering

--- relayer/processor/path_end.go
+++ relayer/processor/path_end.go
@@ -1,14 +1,83 @@
 package processor
 
+// PathEnd references one chain involved in a path.
+// A path is composed of two PathEnds.
 type PathEnd struct {
-	ChainID      string
-	ClientID     string
-	ConnectionID string
+	ChainID  string
+	ClientID string
+	// ConnectionIDs are tracked by pathEndRuntime in PathProcessor for known connections on this client
 
-	// TODO channel filter stuff (allowlist, denylist)
+	// can only provide one. panic if both are provided
+	AllowList []ChannelKey // only use these if provided
+	BlockList []ChannelKey // use everything except these if provided
 }
 
+// NewPathEnd constructs a PathEnd, validating initial parameters.
+func NewPathEnd(chainID string, clientID string, allowList []ChannelKey, blockList []ChannelKey) PathEnd {
+	if len(allowList) > 0 && len(blockList) > 0 {
+		panic("only one of allowlist or blocklist are allowed")
+	}
+	return PathEnd{
+		ChainID:   chainID,
+		ClientID:  clientID,
+		AllowList: allowList,
+		BlockList: blockList,
+	}
+}
+
+func (pe PathEnd) checkChannelMatch(listChannelID, listPortID string, channelKey ChannelKey) bool {
+	if listChannelID == "" {
+		return false
+	}
+	if listChannelID == channelKey.ChannelID {
+		if listPortID == "" {
+			return true
+		}
+		if listPortID == channelKey.PortID {
+			return true
+		}
+	}
+	if listChannelID == channelKey.CounterpartyChannelID {
+		if listPortID == "" {
+			return true
+		}
+		if listPortID == channelKey.CounterpartyPortID {
+			return true
+		}
+	}
+	return false
+}
+
+func (pe PathEnd) shouldRelayChannelSingle(channelKey ChannelKey, listChannel ChannelKey, allowList bool) bool {
+	if pe.checkChannelMatch(listChannel.ChannelID, listChannel.PortID, channelKey) {
+		return allowList
+	}
+	if pe.checkChannelMatch(listChannel.CounterpartyChannelID, listChannel.CounterpartyPortID, channelKey) {
+		return allowList
+	}
+	return !allowList
+}
+
+// if port ID is empty on allowlist channel, allow all ports
+// if port ID is non-empty on allowlist channel, allow only that specific port
+// if port ID is empty on blocklist channel, block all ports
+// if port ID is non-empty on blocklist channel, block only that specific port
 func (pe PathEnd) ShouldRelayChannel(channelKey ChannelKey) bool {
-	// TODO return based on channel filter
+	if len(pe.AllowList) > 0 {
+		for _, allowedChannel := range pe.AllowList {
+			if pe.shouldRelayChannelSingle(channelKey, allowedChannel, true) {
+				return true
+			}
+		}
+		return false
+	} else if len(pe.BlockList) > 0 {
+		for _, blockedChannel := range pe.BlockList {
+			if !pe.shouldRelayChannelSingle(channelKey, blockedChannel, false) {
+				return false
+			}
+		}
+		return true
+	}
+	// if neither allow list or block list are provided, all channels are okay
 	return true
 }
--- relayer/processor/path_end_runtime.go
+++ relayer/processor/path_end_runtime.go
@@ -8,6 +8,14 @@ import "github.com/cosmos/relayer/v2/relayer/provider"
 type pathEndRuntime struct {
 	info PathEnd
 
+	// TODO populate known connections and channels as they are discovered on this client.
+	// channel handshakes will not work until these are populated properly.
+
+	// includes all known and discovered connections that use this PathEnd's client.
+	knownConnections []string
+	// includes all known and discovered channels on connections that use this PathEnd's client.
+	knownChannels []string
+
 	chainProvider provider.ChainProvider
 
 	// cached data
@@ -44,20 +52,76 @@ func newPathEndRuntime(pathEnd PathEnd) *pathEndRuntime {
 	}
 }
 
+func (pathEnd *pathEndRuntime) isRelevantConnection(connectionID string) bool {
+	for _, conn := range pathEnd.knownConnections {
+		if conn == connectionID {
+			return true
+		}
+	}
+	return false
+}
+
+// mergeMessageCache merges relevant IBC messages for packet flows, connection handshakes, and channel handshakes.
+func (pathEnd *pathEndRuntime) mergeMessageCache(messageCache IBCMessagesCache) {
+	packetMessages := make(ChannelPacketMessagesCache)
+	connectionHandshakeMessages := make(ConnectionMessagesCache)
+	channelHandshakeMessages := make(ChannelMessagesCache)
+
+	for ch, pmc := range messageCache.PacketFlow {
+		if !pathEnd.info.ShouldRelayChannel(ch) {
+			packetMessages[ch] = pmc
+		}
+	}
+	pathEnd.messageCache.PacketFlow.Merge(packetMessages)
+
+	for action, cmc := range messageCache.ConnectionHandshake {
+		newCmc := make(ConnectionMessageCache)
+		for conn, msg := range cmc {
+			if pathEnd.info.ClientID == conn.ClientID {
+				// can complete connection handshakes on this client
+				// since PathProcessor holds reference to the counterparty chain pathEndRuntime.
+				newCmc[conn] = msg
+				break
+			}
+		}
+		if len(newCmc) == 0 {
+			continue
+		}
+		connectionHandshakeMessages[action] = newCmc
+	}
+	pathEnd.messageCache.ConnectionHandshake.Merge(connectionHandshakeMessages)
+
+	for action, cmc := range messageCache.ChannelHandshake {
+		newCmc := make(ChannelMessageCache)
+		for ch, msg := range cmc {
+			for _, chID := range pathEnd.knownChannels {
+				if chID == ch.ChannelID {
+					// if channel's connection is known, and connection is on top of this client,
+					// then we can complete channel handshake for this channel
+					// since PathProcessor holds reference to the counterparty chain pathEndRuntime.
+					newCmc[ch] = msg
+					break
+				}
+			}
+		}
+		if len(newCmc) == 0 {
+			continue
+		}
+		channelHandshakeMessages[action] = newCmc
+	}
+	pathEnd.messageCache.ChannelHandshake.Merge(channelHandshakeMessages)
+}
+
 func (pathEnd *pathEndRuntime) MergeCacheData(d ChainProcessorCacheData) {
 	pathEnd.inSync = d.InSync
 	pathEnd.latestBlock = d.LatestBlock
 	pathEnd.latestHeader = d.LatestHeader
 	pathEnd.clientState = d.ClientState
 
-	// TODO messageCache Merge will likely need to move into the PathProcessor, and make sure passes client, connection, or channel filter
-	pathEnd.messageCache.Merge(d.IBCMessagesCache) // Merge incoming packet IBC messages into the backlog
+	pathEnd.mergeMessageCache(d.IBCMessagesCache) // Merge incoming packet IBC messages into the backlog
 
-	// TODO connectionStateCache Merge will likely need to move into the PathProcessor, and make sure passes connection filter
 	pathEnd.connectionStateCache.Merge(d.ConnectionStateCache) // Update latest connection open state for chain
-
-	// TODO channelStateCache Merge will likely need to move into the PathProcessor, and make sure passes connection filter
-	pathEnd.channelStateCache.Merge(d.ChannelStateCache) // Update latest channel open state for chain
+	pathEnd.channelStateCache.Merge(d.ChannelStateCache)       // Update latest channel open state for chain
 
 	pathEnd.ibcHeaderCache.Merge(d.IBCHeaderCache)  // Update latest IBC header state
 	pathEnd.ibcHeaderCache.Prune(ibcHeadersToCache) // Only keep most recent IBC headers
