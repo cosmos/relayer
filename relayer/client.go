cb40140b0891056cae289b19e1c8bc437f95f0da
Dan Kanefsky <daniel.kanefsky@gmail.com>
2022-07-26 19:03:55 -0700

    remove extra print

--- relayer/client.go
+++ relayer/client.go
@@ -108,7 +108,6 @@ func CreateClient(ctx context.Context, src, dst *Chain, srcUpdateHeader, dstUpda
 
 	// Query the trusting period for dst and retry if the query fails
 	var tp time.Duration
-	fmt.Println("DAN DURATION:", customClientTrustingPeriod)
 	if customClientTrustingPeriod == 0 {
 		if err := retry.Do(func() error {
 			var err error
