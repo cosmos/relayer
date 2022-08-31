7cee4648a9a7ffe1a1f98c5c32ae824577f517b6
jtieri <justin@thetieris.com>
2022-08-31 15:32:31 -0500

    undo previous changes regarding purging cache on max retries

--- relayer/processor/path_end_runtime.go
+++ relayer/processor/path_end_runtime.go
@@ -507,6 +507,9 @@ func (pathEnd *pathEndRuntime) shouldSendChannelMessage(message channelIBCMessag
 			toDelete[chantypes.EventTypeChannelOpenTry] = []ChannelKey{channelKey}
 			toDeleteCounterparty[chantypes.EventTypeChannelOpenInit] = []ChannelKey{counterpartyKey.MsgInitKey()}
 		case chantypes.EventTypeChannelCloseConfirm:
+			toDeleteCounterparty[chantypes.EventTypeChannelCloseInit] = []ChannelKey{counterpartyKey}
+			toDelete[chantypes.EventTypeChannelCloseConfirm] = []ChannelKey{channelKey}
+
 			// Gather relevant send packet messages, for this channel key, that should be deleted if we
 			// are operating on an ordered channel.
 			if messageCache, ok := pathEnd.messageCache.PacketFlow[channelKey]; ok {
@@ -536,6 +539,10 @@ func (pathEnd *pathEndRuntime) shouldSendChannelMessage(message channelIBCMessag
 		pathEnd.channelProcessing.deleteMessages(map[string][]ChannelKey{eventType: {channelKey}})
 		pathEnd.messageCache.PacketFlow[channelKey].DeleteMessages(toDeletePacket)
 		//counterparty.messageCache.PacketFlow[counterpartyKey].DeleteMessages(toDeleteCounterpartyPacket)
+
+		// delete all connection handshake retention history for this channel
+		pathEnd.messageCache.ChannelHandshake.DeleteMessages(toDelete)
+		counterparty.messageCache.ChannelHandshake.DeleteMessages(toDeleteCounterparty)
 
 		return false
 	}
