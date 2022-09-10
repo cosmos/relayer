d8d9303a6bf97b1aa9c683944a61e91f1454d7ce
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2022-09-09 21:46:41 -0600

    Update IBCdata struct
    Update IBCdata struct based on recent changes made at the Cosmos Chain Registry

--- relayer/path.go
+++ relayer/path.go
@@ -105,24 +105,24 @@ type ChannelFilter struct {
 type IBCdata struct {
 	Schema string `json:"$schema"`
 	Chain1 struct {
-		ChainName    string `json:"chain-name"`
-		ClientID     string `json:"client-id"`
-		ConnectionID string `json:"connection-id"`
-	} `json:"chain-1"`
+		ChainName    string `json:"chain_name"`
+		ClientID     string `json:"client_id"`
+		ConnectionID string `json:"connection_id"`
+	} `json:"chain_1"`
 	Chain2 struct {
-		ChainName    string `json:"chain-name"`
-		ClientID     string `json:"client-id"`
-		ConnectionID string `json:"connection-id"`
-	} `json:"chain-2"`
+		ChainName    string `json:"chain_name"`
+		ClientID     string `json:"client_id"`
+		ConnectionID string `json:"connection_id"`
+	} `json:"chain_2"`
 	Channels []struct {
 		Chain1 struct {
-			ChannelID string `json:"channel-id"`
-			PortID    string `json:"port-id"`
-		} `json:"chain-1"`
+			ChannelID string `json:"channel_id"`
+			PortID    string `json:"port_id"`
+		} `json:"chain_1"`
 		Chain2 struct {
-			ChannelID string `json:"channel-id"`
-			PortID    string `json:"port-id"`
-		} `json:"chain-2"`
+			ChannelID string `json:"channel_id"`
+			PortID    string `json:"port_id"`
+		} `json:"chain_2"`
 		Ordering string `json:"ordering"`
 		Version  string `json:"version"`
 		Tags     struct {
