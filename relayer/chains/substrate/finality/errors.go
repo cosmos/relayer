9c93bfca5c46d3ace71d651b815161ec7061d78e
Osho Emmanuel <oshoklinsmann@gmail.com>
2022-12-07 15:53:28 +0100

    move beefy client state and header construction to finality module

---
+++ relayer/chains/substrate/finality/errors.go
@@ -0,0 +1,4 @@
+package finality
+
+const ErrParachainSetNotFound = "parachain set not found"
+const ErrAuthoritySetNotFound = "authority set not found"
--- relayer/chains/substrate/finality/finality.go
+++ relayer/chains/substrate/finality/finality.go
@@ -2,23 +2,27 @@ package finality
 
 import (
 	"fmt"
+
+	"github.com/ChainSafe/chaindb"
 	rpcclient "github.com/ComposableFi/go-substrate-rpc-client/v4"
+	rpcclienttypes "github.com/ComposableFi/go-substrate-rpc-client/v4/types"
 	ibcexported "github.com/cosmos/ibc-go/v5/modules/core/exported"
 	"github.com/cosmos/relayer/v2/relayer/chains/substrate"
-	"github.com/cosmos/relayer/v2/relayer/provider"
 )
 
 type FinalityGadget interface {
-	ClientState(provider.IBCHeader) (ibcexported.ClientState, error)
+	ClientState(rpcclienttypes.SignedCommitment) (ibcexported.ClientState, error)
+	Header(blockHash rpcclienttypes.Hash, previousFinalizedHash *rpcclienttypes.Hash) (ibcexported.Header, error)
 }
 
 func NewFinalityGadget(
 	substrateCfg substrate.SubstrateProviderConfig,
 	parachainClient, relayChainClient *rpcclient.SubstrateAPI,
+	memDB *chaindb.BadgerDB,
 ) (FinalityGadget, error) {
 	switch substrateCfg.FinalityGadget {
 	case BeefyFinalityGadget:
-		return NewBeefy(substrateCfg, parachainClient, relayChainClient), nil
+		return NewBeefy(substrateCfg, parachainClient, relayChainClient, memDB), nil
 	default:
 		return nil, fmt.Errorf("unsupported finality gadget")
 	}
