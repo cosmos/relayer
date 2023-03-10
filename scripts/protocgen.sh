5b295b4f3d84ff10c54269ec0948abf11f14371a
Andrew Gouin <andrew@gouin.io>
2023-03-10 13:47:25 -0700

    Penumbra buf go gen

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -15,8 +15,13 @@ for dir in $proto_dirs; do
   done
 done
 
+buf generate --template proto/buf.gen.gogo.yaml buf.build/penumbra-zone/penumbra
+
 # move proto files to the right places
 #
 # Note: Proto files are suffixed with the current binary version.
 cp -r github.com/cosmos/relayer/v2/* ./
 rm -rf github.com
+
+cp -r penumbra/* relayer/chains/penumbra/
+rm -rf penumbra
