2dced631a2241595eae97dd94bd72f5d6869357c
Giuseppe Valente <giuseppe@polymerlabs.org>
2023-03-31 17:01:58 -0700

    test: single hop test with wasm and osmosis

--- interchaintest/multihop_test.go
+++ interchaintest/multihop_test.go
@@ -13,12 +13,20 @@ import (
 	"github.com/strangelove-ventures/interchaintest/v7/testutil"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
-	"golang.org/x/sync/errgroup"
 )
 
 func wasmChainSpec(suffix string, nv, nf int) *interchaintest.ChainSpec {
 	chainName := "wasm"
 	chainID := "wasm" + suffix
+	denom := "uand"
+	switch suffix {
+	case "-1":
+		denom += "one"
+	case "-2":
+		denom += "two"
+	default:
+		panic("invalid suffix")
+	}
 	return &interchaintest.ChainSpec{
 		Name:          chainName,
 		ChainName:     chainID,
@@ -38,9 +46,9 @@ func wasmChainSpec(suffix string, nv, nf int) *interchaintest.ChainSpec {
 			},
 			Bin:            "wasmd",
 			Bech32Prefix:   "wasm",
-			Denom:          "uand",
+			Denom:          denom,
 			GasAdjustment:  1.3,
-			GasPrices:      "0.0uand",
+			GasPrices:      "0.0" + denom,
 			TrustingPeriod: "336h",
 		},
 	}
@@ -110,6 +118,142 @@ func TestOsmosisBuild(t *testing.T) {
 	})
 }
 
+func TestRelayerPathWithWasm(t *testing.T) {
+	var (
+		r    = relayerinterchaintest.NewRelayer(t, relayerinterchaintest.RelayerConfig{})
+		rep  = testreporter.NewNopReporter()
+		eRep = rep.RelayerExecReporter(t)
+		ctx  = context.Background()
+		nv   = 1
+		nf   = 0
+	)
+	// Define chains involved in test
+	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
+		wasmChainSpec("-1", nv, nf),
+		{
+			Name:          "osmosis",
+			ChainName:     "osmosis",
+			Version:       "v11.0.1",
+			NumValidators: &nv,
+			NumFullNodes:  &nf,
+		},
+	})
+
+	chains, err := cf.Chains(t.Name())
+	require.NoError(t, err)
+
+	wasm, osmosis := chains[0].(*cosmos.CosmosChain), chains[1].(*cosmos.CosmosChain)
+	wasmCfg, osmosisCfg := wasm.Config(), osmosis.Config()
+
+	// Build the network; spin up the chains and configure the relayer
+	const pathWasmOsmosis = "wasm-osmosis"
+	const relayerName = "relayer"
+
+	// TODO: create connections but not channels
+	ic := interchaintest.NewInterchain().
+		AddChain(wasm).
+		AddChain(osmosis).
+		AddRelayer(r, relayerName).
+		AddLink(interchaintest.InterchainLink{
+			Chain1:  wasm,
+			Chain2:  osmosis,
+			Relayer: r,
+			Path:    pathWasmOsmosis,
+		})
+
+	client, network := interchaintest.DockerSetup(t)
+
+	require.NoError(t, ic.Build(ctx, eRep, interchaintest.InterchainBuildOptions{
+		TestName:          t.Name(),
+		Client:            client,
+		NetworkID:         network,
+		BlockDatabaseFile: interchaintest.DefaultBlockDatabaseFilepath(),
+
+		SkipPathCreation: false,
+	}))
+	t.Cleanup(func() {
+		_ = ic.Close()
+	})
+
+	// Start the relayers
+	err = r.StartRelayer(ctx, eRep, pathWasmOsmosis)
+	require.NoError(t, err)
+
+	t.Cleanup(
+		func() {
+			err := r.StopRelayer(ctx, eRep)
+			if err != nil {
+				t.Logf("an error occured while stopping the relayer: %s", err)
+			}
+		},
+	)
+
+	// Fund user accounts, so we can query balances and make assertions.
+	const userFunds = int64(10_000_000)
+	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, wasm, osmosis)
+	wasmUser, osmosisUser := users[0].(*cosmos.CosmosWallet), users[1].(*cosmos.CosmosWallet)
+
+	// Wait a few blocks for user accounts to be created on chain.
+	err = testutil.WaitForBlocks(ctx, 5, wasm, osmosis)
+	require.NoError(t, err)
+
+	wasmAddress := wasmUser.FormattedAddress()
+	require.NotEmpty(t, wasmAddress)
+
+	osmosisAddress := osmosisUser.FormattedAddress()
+	require.NotEmpty(t, osmosisAddress)
+
+	// get ibc chans
+	wasmChans, err := r.GetChannels(ctx, eRep, wasmCfg.ChainID)
+	require.NoError(t, err)
+	require.Len(t, wasmChans, 1)
+
+	osmosisChans, err := r.GetChannels(ctx, eRep, osmosisCfg.ChainID)
+	require.NoError(t, err)
+	require.Len(t, osmosisChans, 1)
+
+	wasmIBCDenom := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom(wasmChans[0].Counterparty.PortID, wasmChans[0].Counterparty.ChannelID, wasmCfg.Denom)).IBCDenom()
+	osmosisIBCDenom := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom(osmosisChans[0].Counterparty.PortID, osmosisChans[0].Counterparty.ChannelID, osmosisCfg.Denom)).IBCDenom()
+
+	const transferAmount = int64(1_000_000)
+
+	wasmHeight, err := wasm.Height(ctx)
+	require.NoError(t, err)
+
+	// Fund osmosis user with ibc denom wasm1
+	tx, err := wasm.SendIBCTransfer(ctx, wasmChans[0].ChannelID, wasmUser.KeyName(), ibc.WalletAmount{
+		Amount:  transferAmount,
+		Denom:   wasmCfg.Denom,
+		Address: osmosisAddress,
+	}, ibc.TransferOptions{})
+	require.NoError(t, err)
+	_, err = testutil.PollForAck(ctx, wasm, wasmHeight, wasmHeight+10, tx.Packet)
+	require.NoError(t, err)
+
+	osmosisHeight, err := osmosis.Height(ctx)
+	require.NoError(t, err)
+
+	// Fund wasm user with ibc denom osmosis
+	tx, err = osmosis.SendIBCTransfer(ctx, osmosisChans[0].ChannelID, osmosisUser.KeyName(), ibc.WalletAmount{
+		Amount:  transferAmount,
+		Denom:   osmosisCfg.Denom,
+		Address: wasmAddress,
+	}, ibc.TransferOptions{})
+	require.NoError(t, err)
+	_, err = testutil.PollForAck(ctx, osmosis, osmosisHeight, osmosisHeight+10, tx.Packet)
+	require.NoError(t, err)
+
+	wasmOnOsmosisBalance, err := osmosis.GetBalance(ctx, osmosisAddress, wasmIBCDenom)
+	require.NoError(t, err)
+
+	require.Equal(t, transferAmount, wasmOnOsmosisBalance)
+
+	osmosisOnWasmBalance, err := wasm.GetBalance(ctx, wasmAddress, osmosisIBCDenom)
+	require.NoError(t, err)
+
+	require.Equal(t, transferAmount, osmosisOnWasmBalance)
+}
+
 func TestRelayerMultihop(t *testing.T) {
 	var (
 		r    = relayerinterchaintest.NewRelayer(t, relayerinterchaintest.RelayerConfig{})
@@ -136,7 +280,6 @@ func TestRelayerMultihop(t *testing.T) {
 	require.NoError(t, err)
 
 	wasm1, osmosis, wasm2 := chains[0].(*cosmos.CosmosChain), chains[1].(*cosmos.CosmosChain), chains[2].(*cosmos.CosmosChain)
-	wasm1Cfg, osmosisCfg, wasm2Cfg := wasm1.Config(), osmosis.Config(), wasm2.Config()
 
 	// Build the network; spin up the chains and configure the relayer
 	const pathWasm1Osmosis = "wasm1-osmosis"
@@ -207,72 +350,5 @@ func TestRelayerMultihop(t *testing.T) {
 	wasm2Address := wasm2User.FormattedAddress()
 	require.NotEmpty(t, wasm2Address)
 
-	// get ibc chans
-	wasm1Chans, err := r.GetChannels(ctx, eRep, wasm1Cfg.ChainID)
-	require.NoError(t, err)
-	require.Len(t, wasm1Chans, 1)
-
-	osmosisChans, err := r.GetChannels(ctx, eRep, osmosisCfg.ChainID)
-	require.NoError(t, err)
-	// TODO: do not create direct channels
-	require.Len(t, osmosisChans, 2)
-
-	wasm2Chans, err := r.GetChannels(ctx, eRep, wasm2Cfg.ChainID)
-	require.NoError(t, err)
-	require.Len(t, wasm2Chans, 1)
-
-	wasm1IBCDenom := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom(wasm1Chans[0].Counterparty.PortID, wasm1Chans[0].Counterparty.ChannelID, wasm1Cfg.Denom)).IBCDenom()
-	wasm2IBCDenom := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom(wasm2Chans[0].Counterparty.PortID, wasm2Chans[0].Counterparty.ChannelID, wasm2Cfg.Denom)).IBCDenom()
-
-	var eg errgroup.Group
-
-	const transferAmount = int64(1_000_000)
-
-	eg.Go(func() error {
-		wasm1Height, err := wasm1.Height(ctx)
-		if err != nil {
-			return err
-		}
-		// Fund gaia user with ibc denom osmo
-		tx, err := wasm1.SendIBCTransfer(ctx, wasm1Chans[0].ChannelID, wasm2User.KeyName(), ibc.WalletAmount{
-			Amount:  transferAmount,
-			Denom:   wasm1Cfg.Denom,
-			Address: wasm2Address,
-		}, ibc.TransferOptions{})
-		if err != nil {
-			return err
-		}
-		_, err = testutil.PollForAck(ctx, osmosis, wasm1Height, wasm1Height+10, tx.Packet)
-		return err
-	})
-
-	eg.Go(func() error {
-		wasm2Height, err := wasm2.Height(ctx)
-		if err != nil {
-			return err
-		}
-		// Fund gaia user with ibc denom juno
-		tx, err := wasm2.SendIBCTransfer(ctx, wasm2Chans[0].ChannelID, wasm2User.KeyName(), ibc.WalletAmount{
-			Amount:  transferAmount,
-			Denom:   wasm2Cfg.Denom,
-			Address: wasm1Address,
-		}, ibc.TransferOptions{})
-		if err != nil {
-			return err
-		}
-		_, err = testutil.PollForAck(ctx, wasm2, wasm2Height, wasm2Height+10, tx.Packet)
-		return err
-	})
-
-	require.NoError(t, eg.Wait())
-
-	wasm1OnWasm2Balance, err := wasm2.GetBalance(ctx, wasm2Address, wasm1IBCDenom)
-	require.NoError(t, err)
-
-	require.Equal(t, transferAmount, wasm1OnWasm2Balance)
-
-	wasm2OnWasm1Balance, err := wasm1.GetBalance(ctx, wasm1Address, wasm2IBCDenom)
-	require.NoError(t, err)
-
-	require.Equal(t, transferAmount, wasm2OnWasm1Balance)
+	// TODO: create multihop channel
 }
