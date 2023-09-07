b5dce36a3ea211220ba290ddf203b9c76e575add
vimystic <122659254+vimystic@users.noreply.github.com>
2023-09-07 17:49:56 -0600

    Add use command to "rly keys" (#1282)

--- cmd/keys.go
+++ cmd/keys.go
@@ -45,6 +45,7 @@ func keysCmd(a *appState) *cobra.Command {
 
 	cmd.AddCommand(
 		keysAddCmd(a),
+		keysUseCmd(a),
 		keysRestoreCmd(a),
 		keysDeleteCmd(a),
 		keysListCmd(a),
@@ -52,6 +53,22 @@ func keysCmd(a *appState) *cobra.Command {
 		keysShowCmd(a),
 	)
 
+	return cmd
+}
+
+func keysUseCmd(a *appState) *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:     "use chain_name key_name",
+		Aliases: []string{"a"},
+		Short:   "Use a key from the keychain associated with a particular chain. Look at ~/.relayer/keys/ibc-0/keyring-test ",
+		Args:    withUsage(cobra.ExactArgs(2)),
+		Example: strings.TrimSpace(fmt.Sprintf(`
+$ %s keys use ibc-0 key_name`, appName)),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return a.useKey(args[0], args[1])
+		},
+	}
 	return cmd
 }
 
