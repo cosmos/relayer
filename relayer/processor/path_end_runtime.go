8b7184bfbbb6def03dce9dc3650beac81d18efca
Andrew Gouin <andrew@gouin.io>
2024-02-01 09:04:35 -0700

    use counterparty key for remove packet retention

--- relayer/processor/path_end_runtime.go
+++ relayer/processor/path_end_runtime.go
@@ -615,6 +615,7 @@ func (pathEnd *pathEndRuntime) removePacketRetention(
 		toDelete[preInitKey] = []uint64{sequence}
 	case chantypes.EventTypeRecvPacket:
 		toDelete[eventType] = []uint64{sequence}
+		toDelete[chantypes.EventTypeWriteAck] = []uint64{sequence}
 		toDeleteCounterparty[chantypes.EventTypeSendPacket] = []uint64{sequence}
 	case chantypes.EventTypeAcknowledgePacket:
 		toDelete[eventType] = []uint64{sequence}
@@ -625,13 +626,15 @@ func (pathEnd *pathEndRuntime) removePacketRetention(
 		toDelete[eventType] = []uint64{sequence}
 		toDelete[chantypes.EventTypeSendPacket] = []uint64{sequence}
 	}
+
 	// delete in progress send for this specific message
 	pathEnd.packetProcessing[k].deleteMessages(map[string][]uint64{
 		eventType: {sequence},
 	})
+
 	// delete all packet flow retention history for this sequence
 	pathEnd.messageCache.PacketFlow[k].DeleteMessages(toDelete)
-	counterparty.messageCache.PacketFlow[k].DeleteMessages(toDeleteCounterparty)
+	counterparty.messageCache.PacketFlow[k.Counterparty()].DeleteMessages(toDeleteCounterparty)
 }
 
 // shouldSendConnectionMessage determines if the connection handshake message should be sent now.
