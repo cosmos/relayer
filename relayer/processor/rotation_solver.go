f6b96cce8d2397fb1ffd8922dbc6b3f5a7c9138b
danwt <30197399+danwt@users.noreply.github.com>
2024-11-13 16:47:50 +0000

    fix some errors, pray

--- relayer/processor/rotation_solver.go
+++ relayer/processor/rotation_solver.go
@@ -28,8 +28,9 @@ func (s *rotationSolver) solve(ctx c.Context) error {
 
 	h, preRotationValhash, err := s.hubClientValset(ctx)
 	if err != nil {
-		return fmt.Errorf("hub client valset: %w", err)
+		return fmt.Errorf("get initial hub client valset: %w", err)
 	}
+
 	// sanity check to make sure that the rollapp actually has a different val set (confirm there is a problem)
 	if bytes.Equal(s.ra.latestHeader.NextValidatorsHash(), preRotationValhash) {
 		return errFalsePositive
@@ -37,66 +38,27 @@ func (s *rotationSolver) solve(ctx c.Context) error {
 
 	rollappHeaders, err := s.rollappHeaders(ctx, h, preRotationValhash)
 	if err != nil {
-		return fmt.Errorf("rollapp headers: %w", err)
+		return fmt.Errorf("search for two rollapp headers: %w", err)
 	}
 
 	err = s.sendUpdates(ctx, rollappHeaders[0], rollappHeaders[1])
 	if err != nil {
-		return fmt.Errorf("send updates: %w", err)
+		return fmt.Errorf("send the two headers in updates: %w", err)
 	}
 	return nil
 }
 
-// a = h, b = h+1 where valhash changes in between
-func (s *rotationSolver) sendUpdates(ctx c.Context, a, b provider.IBCHeader) error {
+func (s *rotationSolver) hubClientValset(ctx c.Context) (uint64, []byte, error) {
 
-	u1, err := s.ra.chainProvider.MsgUpdateClientHeader(
-		a, // header to send in update
-
-		// here we assume by this code we can reconstruct the trust
-		// https://github.com/dymensionxyz/go-relayer/blob/838f324793473de99cbf285f66537580c4158f39/relayer/processor/message_processor.go#L309-L316
-		s.hub.clientState.ConsensusHeight,  // trust height
-		s.hub.clientTrustedState.IBCHeader, // trust header. Should be trust height + 1 in theory
-	)
+	h := s.hub.clientState.ConsensusHeight.GetRevisionHeight()
+	header, err := s.ra.chainProvider.QueryIBCHeader(ctx, int64(h))
 	if err != nil {
-		return fmt.Errorf("msg update client header: %w", err)
+		return 0, nil, fmt.Errorf("query ibc header: %w", err)
 	}
-
-	mu1, err := s.hub.chainProvider.MsgUpdateClient(s.hub.info.ClientID, u1)
-	if err != nil {
-		return fmt.Errorf("msg update client: %w", err)
+	if header.Height() != h {
+		return 0, nil, fmt.Errorf("header height mismatch: got %d, expected %d", header.Height(), h)
 	}
-
-	aHeight := clienttypes.Height{
-		RevisionNumber: s.hub.clientState.ConsensusHeight.RevisionNumber,
-		RevisionHeight: a.Height(),
-	}
-	s.hub.clientState.ConsensusHeight.RevisionHeight = 4
-
-	u2, err := s.ra.chainProvider.MsgUpdateClientHeader(
-		b,
-		aHeight,
-		b, // use b as a trust basis for itself, pretty sure this should work
-	)
-	if err != nil {
-		return fmt.Errorf("msg update client header: %w", err)
-	}
-
-	mu2, err := s.hub.chainProvider.MsgUpdateClient(s.hub.info.ClientID, u2)
-	if err != nil {
-		return fmt.Errorf("msg update client: %w", err)
-	}
-
-	if err := s.broadcastUpdates(ctx, []provider.RelayerMessage{mu1, mu2}); err != nil {
-		return fmt.Errorf("broadcast updates: %w", err)
-	}
-	return nil
-}
-
-func (s *rotationSolver) broadcastUpdates(ctx c.Context, msgs []provider.RelayerMessage) error {
-	broadcastCtx, cancel := c.WithTimeout(ctx, messageSendTimeout)
-	defer cancel()
-	return s.hub.chainProvider.SendMessagesToMempool(broadcastCtx, msgs, " ", ctx, nil)
+	return header.Height(), header.NextValidatorsHash(), nil
 }
 
 // finds the two headers where it changes from hValhash to a different one
@@ -109,7 +71,7 @@ func (s *rotationSolver) rollappHeaders(ctx c.Context, hHub uint64, hubValHash [
 		hQ := int64(ansCandidate) - 1
 		headerSub1, err := s.ra.chainProvider.QueryIBCHeader(ctx, hQ)
 		if err != nil {
-			return 0, fmt.Errorf("query ibc header candidate sub 1 hq: %d: %w", hQ, err)
+			return 0, fmt.Errorf("query ibc header candidate h sub 1: %d: %w", hQ, err)
 		}
 		if !bytes.Equal(headerSub1.NextValidatorsHash(), hubValHash) {
 			// too high
@@ -118,7 +80,7 @@ func (s *rotationSolver) rollappHeaders(ctx c.Context, hHub uint64, hubValHash [
 		hQ = int64(ansCandidate)
 		header, err := s.ra.chainProvider.QueryIBCHeader(ctx, hQ)
 		if err != nil {
-			return 0, fmt.Errorf("query ibc header candidate hq: %d: %w", hQ, err)
+			return 0, fmt.Errorf("query ibc header candidate h: %d: %w", hQ, err)
 		}
 		if !bytes.Equal(header.NextValidatorsHash(), hubValHash) {
 			// perfect: this is the FIRST header with a different nextValidatorsHash
@@ -134,10 +96,13 @@ func (s *rotationSolver) rollappHeaders(ctx c.Context, hHub uint64, hubValHash [
 		return nil, fmt.Errorf("search: %w", err)
 	}
 
+	// last header produced by old sequencer
 	a, err := s.ra.chainProvider.QueryIBCHeader(ctx, int64(ans))
 	if err != nil {
 		return nil, fmt.Errorf("query ibc header a : h: %d: %w", ans, err)
 	}
+
+	// first header produced by new sequencer
 	b, err := s.ra.chainProvider.QueryIBCHeader(ctx, int64(ans+1))
 	if err != nil {
 		return nil, fmt.Errorf("query ibc header b : h: %d: %w", ans+1, err)
@@ -156,28 +121,66 @@ func search(sleep time.Duration, l, r uint64, direction func(uint64) (int, error
 			return 0, fmt.Errorf("direction: l: %d, m: %d, r: %d, : %w", l, m, r, err)
 		}
 		if d < 0 {
-			r = m
+			r = m - 1 // TODO: check
 		}
 		if d == 0 {
 			return m, nil
 		}
 		if 0 < d {
-			l = m
+			l = m + 1 // TODO: check
 		}
 		time.Sleep(sleep)
 	}
 	return 0, fmt.Errorf("%w: l: %d, r: %d, ", errTargetNotFound, l, r)
 }
 
-func (s *rotationSolver) hubClientValset(ctx c.Context) (uint64, []byte, error) {
+// a = h, b = h+1 where valhash changes in between
+func (s *rotationSolver) sendUpdates(ctx c.Context, a, b provider.IBCHeader) error {
 
-	h := s.hub.clientState.ConsensusHeight.GetRevisionHeight()
-	header, err := s.ra.chainProvider.QueryIBCHeader(ctx, int64(h))
+	u1, err := s.ra.chainProvider.MsgUpdateClientHeader(
+		a, // header to send in update
+
+		// here we assume by this code we can reconstruct the trust
+		// https://github.com/dymensionxyz/go-relayer/blob/838f324793473de99cbf285f66537580c4158f39/relayer/processor/message_processor.go#L309-L316
+		s.hub.clientState.ConsensusHeight,  // trust height
+		s.hub.clientTrustedState.IBCHeader, // trust header. Should be trust height + 1 in theory
+	)
 	if err != nil {
-		return 0, nil, fmt.Errorf("query ibc header: %w", err)
+		return fmt.Errorf("create msg update client header: %w", err)
 	}
-	if header.Height() != h {
-		return 0, nil, fmt.Errorf("header height mismatch: got %d, expected %d", header.Height(), h)
+
+	mu1, err := s.hub.chainProvider.MsgUpdateClient(s.hub.info.ClientID, u1)
+	if err != nil {
+		return fmt.Errorf("create msg update client: update one %w", err)
 	}
-	return header.Height(), header.NextValidatorsHash(), nil
+
+	aHeight := clienttypes.Height{
+		RevisionNumber: s.hub.clientState.ConsensusHeight.RevisionNumber,
+		RevisionHeight: a.Height(),
+	}
+
+	u2, err := s.ra.chainProvider.MsgUpdateClientHeader(
+		b,       // header to send in update
+		aHeight, // trusted height is now the height of the first header
+		b,       // use b as a trust basis for itself, pretty sure this should work!
+	)
+	if err != nil {
+		return fmt.Errorf("create msg update client header: %w", err)
+	}
+
+	mu2, err := s.hub.chainProvider.MsgUpdateClient(s.hub.info.ClientID, u2)
+	if err != nil {
+		return fmt.Errorf("create msg update client: update two : %w", err)
+	}
+
+	if err := s.broadcastUpdates(ctx, []provider.RelayerMessage{mu1, mu2}); err != nil {
+		return fmt.Errorf("broadcast both updates: %w", err)
+	}
+	return nil
+}
+
+func (s *rotationSolver) broadcastUpdates(ctx c.Context, msgs []provider.RelayerMessage) error {
+	broadcastCtx, cancel := c.WithTimeout(ctx, messageSendTimeout)
+	defer cancel()
+	return s.hub.chainProvider.SendMessagesToMempool(broadcastCtx, msgs, " ", ctx, nil)
 }
